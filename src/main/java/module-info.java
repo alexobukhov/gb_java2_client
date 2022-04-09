216491c0b58b955d657e306a5394403f5ccaff14
Обухов Алексей Михайлович <am.obukhov@vebtech.by>
2022-04-09 14:00:21 +0300

    Project Update

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module gb.safronov.client {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ru.gb.client to javafx.fxml;
+    exports ru.gb.client;
+    exports ru.gb.client.controllers;
+    opens ru.gb.client.controllers to javafx.fxml;
+}
